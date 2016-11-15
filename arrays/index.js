fdbae83ce1d107c54f60d5641d2636f7017e8166
Tyler Garlick <tjgarlick@gmail.com>
2016-11-14 19:00:34 -0700

    Moved arrays to iterators and arrays snippet

--- arrays/index.js
+++ arrays/index.js
@@ -1,309 +1,68 @@
-const person = { firstName: 'Tyler', lastName: 'Garlick', age: 34 };
+// store various types of data in Arrays
 
-const models = [
-  { "model_name": "021 C", "model_make_id": "ford" },
-  {
-    "model_name": "12 M",
-    "model_make_id": "ford"
-  }, { "model_name": "17", "model_make_id": "ford" }, {
-    "model_name": "17M",
-    "model_make_id": "ford"
-  }, { "model_name": "2000", "model_make_id": "ford" }, {
-    "model_name": "24.7",
-    "model_make_id": "ford"
-  }, { "model_name": "427", "model_make_id": "ford" }, {
-    "model_name": "49",
-    "model_make_id": "ford"
-  }, { "model_name": "Aerostar", "model_make_id": "ford" }, {
-    "model_name": "Anglia",
-    "model_make_id": "ford"
-  }, { "model_name": "Artic", "model_make_id": "ford" }, {
-    "model_name": "Aspire",
-    "model_make_id": "ford"
-  }, { "model_name": "Bantam", "model_make_id": "ford" }, {
-    "model_name": "Bronco",
-    "model_make_id": "ford"
-  }, { "model_name": "Bronco II", "model_make_id": "ford" }, {
-    "model_name": "C 100",
-    "model_make_id": "ford"
-  }, { "model_name": "C-MAX", "model_make_id": "ford" }, {
-    "model_name": "C-Max Energi",
-    "model_make_id": "Ford"
-  }, { "model_name": "C-Max Hybrid", "model_make_id": "Ford" }, {
-    "model_name": "Capri",
-    "model_make_id": "ford"
-  }, { "model_name": "Coin", "model_make_id": "ford" }, {
-    "model_name": "Consul",
-    "model_make_id": "ford"
-  }, { "model_name": "Contour", "model_make_id": "ford" }, {
-    "model_name": "Corsair",
-    "model_make_id": "ford"
-  }, { "model_name": "Cortina", "model_make_id": "ford" }, {
-    "model_name": "Cougar",
-    "model_make_id": "ford"
-  }, { "model_name": "Courier", "model_make_id": "ford" }, {
-    "model_name": "Crestline",
-    "model_make_id": "ford"
-  }, { "model_name": "Crown Victoria", "model_make_id": "ford" }, {
-    "model_name": "Custom",
-    "model_make_id": "ford"
-  }, { "model_name": "Desert Excursion", "model_make_id": "ford" }, {
-    "model_name": "E-150",
-    "model_make_id": "ford"
-  }, { "model_name": "E-250", "model_make_id": "ford" }, {
-    "model_name": "E-350",
-    "model_make_id": "ford"
-  }, { "model_name": "e-Ka", "model_make_id": "ford" }, {
-    "model_name": "E-Series",
-    "model_make_id": "ford"
-  }, { "model_name": "E-Series Van", "model_make_id": "Ford" }, {
-    "model_name": "E-Series Wagon",
-    "model_make_id": "Ford"
-  }, { "model_name": "Econoline", "model_make_id": "ford" }, {
-    "model_name": "Econovan",
-    "model_make_id": "ford"
-  }, { "model_name": "Ecosport", "model_make_id": "ford" }, {
-    "model_name": "Edge",
-    "model_make_id": "ford"
-  }, { "model_name": "Equator", "model_make_id": "ford" }, {
-    "model_name": "Escape",
-    "model_make_id": "ford"
-  }, { "model_name": "Escort", "model_make_id": "ford" }, {
-    "model_name": "EX",
-    "model_make_id": "ford"
-  }, { "model_name": "Excursion", "model_make_id": "ford" }, {
-    "model_name": "Expedition",
-    "model_make_id": "ford"
-  }, { "model_name": "Explorer", "model_make_id": "ford" }, {
-    "model_name": "Extreme EX",
-    "model_make_id": "ford"
-  }, { "model_name": "F", "model_make_id": "ford" }, {
-    "model_name": "F-150",
-    "model_make_id": "ford"
-  }, { "model_name": "F-250", "model_make_id": "ford" }, {
-    "model_name": "F-250 Super Duty",
-    "model_make_id": "Ford"
-  }, { "model_name": "F-350", "model_make_id": "ford" }, {
-    "model_name": "F-350 Super Duty",
-    "model_make_id": "Ford"
-  }, { "model_name": "F-450", "model_make_id": "ford" }, {
-    "model_name": "F-450 Super Duty",
-    "model_make_id": "Ford"
-  }, { "model_name": "F-650", "model_make_id": "ford" }, {
-    "model_name": "Fairlane",
-    "model_make_id": "ford"
-  }, { "model_name": "Falcon", "model_make_id": "ford" }, {
-    "model_name": "Festiva",
-    "model_make_id": "ford"
-  }, { "model_name": "Fiesta", "model_make_id": "ford" }, {
-    "model_name": "Five Hundred",
-    "model_make_id": "ford"
-  }, { "model_name": "Flex", "model_make_id": "ford" }, {
-    "model_name": "Focus",
-    "model_make_id": "ford"
-  }, { "model_name": "Focus ST", "model_make_id": "Ford" }, {
-    "model_name": "FPV BF GT",
-    "model_make_id": "ford"
-  }, { "model_name": "Freestar", "model_make_id": "ford" }, {
-    "model_name": "Freestyle",
-    "model_make_id": "ford"
-  }, { "model_name": "Fusion", "model_make_id": "ford" }, {
-    "model_name": "Fusion Energi",
-    "model_make_id": "Ford"
-  }, { "model_name": "Fusion Hybrid", "model_make_id": "Ford" }, {
-    "model_name": "Galaxie",
-    "model_make_id": "ford"
-  }, { "model_name": "Galaxy", "model_make_id": "ford" }, {
-    "model_name": "Gran Torino",
-    "model_make_id": "ford"
-  }, { "model_name": "Granada", "model_make_id": "ford" }, {
-    "model_name": "GT",
-    "model_make_id": "ford"
-  }, { "model_name": "GT 40", "model_make_id": "ford" }, {
-    "model_name": "GT 500",
-    "model_make_id": "ford"
-  }, { "model_name": "GT 70", "model_make_id": "ford" }, {
-    "model_name": "Husky",
-    "model_make_id": "ford"
-  }, { "model_name": "Ikon", "model_make_id": "ford" }, {
-    "model_name": "Indigo",
-    "model_make_id": "ford"
-  }, { "model_name": "Ka", "model_make_id": "ford" }, {
-    "model_name": "Kuga",
-    "model_make_id": "ford"
-  }, { "model_name": "Laser", "model_make_id": "ford" }, {
-    "model_name": "Libre",
-    "model_make_id": "ford"
-  }, { "model_name": "Limited", "model_make_id": "ford" }, {
-    "model_name": "Lotus Cortina",
-    "model_make_id": "ford"
-  }, { "model_name": "LTD", "model_make_id": "ford" }, {
-    "model_name": "Lynx",
-    "model_make_id": "ford"
-  }, { "model_name": "Maverick", "model_make_id": "ford" }, {
-    "model_name": "Megastar II",
-    "model_make_id": "ford"
-  }, { "model_name": "Meteor", "model_make_id": "ford" }, {
-    "model_name": "Model U",
-    "model_make_id": "ford"
-  }, { "model_name": "Monarch", "model_make_id": "ford" }, {
-    "model_name": "Mondeo",
-    "model_make_id": "ford"
-  }, { "model_name": "Mustang", "model_make_id": "ford" }, {
-    "model_name": "O21 C",
-    "model_make_id": "ford"
-  }, { "model_name": "Orion", "model_make_id": "ford" }, {
-    "model_name": "Pilot",
-    "model_make_id": "ford"
-  }, { "model_name": "Popular", "model_make_id": "ford" }, {
-    "model_name": "Prefect",
-    "model_make_id": "ford"
-  }, { "model_name": "Probe", "model_make_id": "ford" }, {
-    "model_name": "Puma",
-    "model_make_id": "ford"
-  }, { "model_name": "Ranchero", "model_make_id": "ford" }, {
-    "model_name": "Ranger",
-    "model_make_id": "ford"
-  }, { "model_name": "Royale", "model_make_id": "ford" }, {
-    "model_name": "RS 200",
-    "model_make_id": "ford"
-  }, { "model_name": "S-Max", "model_make_id": "ford" }, {
-    "model_name": "Saetta",
-    "model_make_id": "ford"
-  }, { "model_name": "Scorpio", "model_make_id": "ford" }, {
-    "model_name": "Shelby GR-1 Concept",
-    "model_make_id": "ford"
-  }, { "model_name": "Shelby GT 500", "model_make_id": "ford" }, {
-    "model_name": "Shelby GT500",
-    "model_make_id": "ford"
-  }, { "model_name": "Sierra", "model_make_id": "ford" }, {
-    "model_name": "Skyliner",
-    "model_make_id": "ford"
-  }, { "model_name": "ST 460", "model_make_id": "ford" }, {
-    "model_name": "Station Wagon",
-    "model_make_id": "ford"
-  }, { "model_name": "Synergy 2010", "model_make_id": "ford" }, {
-    "model_name": "Taunus",
-    "model_make_id": "ford"
-  }, { "model_name": "Taurus", "model_make_id": "ford" }, {
-    "model_name": "Taurus X",
-    "model_make_id": "ford"
-  }, { "model_name": "TE-50", "model_make_id": "ford" }, {
-    "model_name": "Telstar",
-    "model_make_id": "ford"
-  }, { "model_name": "Tempo", "model_make_id": "ford" }, {
-    "model_name": "Territory",
-    "model_make_id": "ford"
-  }, { "model_name": "Think", "model_make_id": "ford" }, {
-    "model_name": "Think Neighbor",
-    "model_make_id": "ford"
-  }, { "model_name": "Thunderbird", "model_make_id": "ford" }, {
-    "model_name": "TL-50",
-    "model_make_id": "ford"
-  }, { "model_name": "Tonka", "model_make_id": "ford" }, {
-    "model_name": "Torino",
-    "model_make_id": "ford"
-  }, { "model_name": "Tracer", "model_make_id": "ford" }, {
-    "model_name": "Transit Connect",
-    "model_make_id": "ford"
-  }, { "model_name": "Transit Van", "model_make_id": "Ford" }, {
-    "model_name": "Transit Wagon",
-    "model_make_id": "Ford"
-  }, { "model_name": "Triton", "model_make_id": "ford" }, {
-    "model_name": "TS-50",
-    "model_make_id": "ford"
-  }, { "model_name": "Urban Explorer", "model_make_id": "ford" }, {
-    "model_name": "Vedette",
-    "model_make_id": "ford"
-  }, { "model_name": "Versailles", "model_make_id": "ford" }, {
-    "model_name": "Windstar",
-    "model_make_id": "ford"
-  }, { "model_name": "XR 8 Xplod", "model_make_id": "ford" }, {
-    "model_name": "Zephyr",
-    "model_make_id": "ford"
-  }, { "model_name": "Zodiac", "model_make_id": "ford" }];
+const list = [1, 'hello', { age: 31 }, true, null, undefined, () => {
+}];
 
 
-// explain and use for, for...in, and for...of statements to interate over an array or object
-
-for (let i = 0; i < models.length; i++) {
-  const model = models[i];
-  // console.log(model.model_name);
-}
-
-const keys = Object.keys(person);
-
-for (let key in person) {
-  // console.log(key);
-}
-
-for (let model of models) {
-  // console.log(model.model_name);
-}
-
-// models.forEach(model => console.log(model.model_name));
-
-
-const combine = (newThing) => [newThing, ...models];
-
-
-// use the while and do... while statements while a condition is true
-let index = 0;
-while (index < models.length) {
-  const model = models[index];
-  // console.log(model.model_name);
-  index++;
-}
-
-
-// do {
-//
-// } while(true);
-
-
-// explain the labeled, break, and continue statements
-// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Statements/label
-
-// let foundColor = '';
-// switch (color) {
-//   case 'green':
-//     return 'green';
-//   case 'red':
-//     foundColor = 'red';
-//     break;
-//   default:
-//     foundColor = 'unknown';
-//     break;
-// }
-
-// Do something with foundColor
-
-
-// nest loops to traverse a multi-dimensional array
-
-const unJaggedArray = [
-  [1, 2, 3],
-  [4, 5, 6],
-  [7, 8, 'other type']
+const people = [
+  { firstName: 'Tyler', lastName: 'Garlick' },
+  { firstName: 'Bob', lastName: 'The Builder' },
+  { firstName: 'Donald', lastName: 'Trump' }
 ];
 
-unJaggedArray.forEach(row => {
-  row.forEach(column => {
-    // console.log(column);
-  });
-});
+
+// retrieve data from precise locations in an Array
+
+// How do we get the first one?
+// const tyler = people[0];
+const tyler = people.slice(0, 1);
 
 
 
-const jaggedArray = [
-  [1, 2, 3], // row 1
-  [4, 5, 6, -1], // row 2
-  [7, 8, 9, 10, 11, 12] // row 3
-];
+// How do we get the last one?
 
-jaggedArray.forEach(row => {
-  console.log(row);
-  row.forEach(column => {
-    console.log(column);
-  });
-});
+// 3 item - 1 = 2
+// const elPresidente = people[2];
+// const elPresidente = people[people.length - 1];
+
+
+
+// retrieve subsets of data from an Array
+
+// last two people
+// const lastTwo = people.slice(1);
+// const lastTwo = people.slice(1, 2);
+
+// const lastTwo = people.filter((person) => {
+//   return person.firstName !== 'Tyler';
+// });
+
+
+
+// combine, copy, add items and subtract from an Array
+
+// people.concat([{ firstName: 'Hillary', lastName: 'Clinton'}]);
+const result = [{ firstName: 'Hillary', lastName: 'Clinton'}, ...people];
+
+
+
+// Copy array
+const copied = [...people];
+
+
+
+// Add
+// people.push({ firstName: 'Hillary', lastName: 'Clinton'});
+// const result = [{ firstName: 'Hillary', lastName: 'Clinton'}, ...people];
+// people.splice(0, 0, { firstName: 'Hillary', lastName: 'Clinton'});
+
+// Removing
+
+// Removes bob the builder
+people.splice(1, 1);
+
+const newPeople = [...people.filter(p => p.firstName !== 'Tyler')];
+
+
+
+
